e87ca86718c5525ef1531459628221bbc8c7ef8e
danilo.silva <danilo.silva@msn.com>
2024-05-26 00:54:17 -0300

    Criando Componente de Listagem

--- UI/codepulse/src/app/features/categoria/categoria-list/categoria-list.component.ts
+++ UI/codepulse/src/app/features/categoria/categoria-list/categoria-list.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoriaService } from '../services/categoria.service';
+import { Categoria } from '../models/categoria.model';
 
 @Component({
   selector: 'app-categoria-list',
   templateUrl: './categoria-list.component.html',
   styleUrls: ['./categoria-list.component.css']
 })
-export class CategoriaListComponent {
+export class CategoriaListComponent implements OnInit {
 
+  categorias?: Categoria[];
+
+  constructor(private categoriaService: CategoriaService) { }
+
+  ngOnInit(): void {
+    this.categoriaService.obterTodasCategorias().subscribe({
+      next: (response) => {
+        this.categorias = response;
+      },
+      error: () => {
+        console.error('Erro ao obter categorias');
+      }
+    });
+  }
 }
