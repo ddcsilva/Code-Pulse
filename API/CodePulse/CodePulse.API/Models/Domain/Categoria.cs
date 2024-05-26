4edcd96b365db849143f2f068ae0e248471a3ba9
danilo.silva <danilo.silva@msn.com>
2024-05-25 21:06:52 -0300

    Criando Classes do Dominio

---
+++ API/CodePulse/CodePulse.API/Models/Domain/Categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace CodePulse.API.Models.Domain;
+
+public class Categoria
+{
+    public Guid Id { get; set; }
+    public string Nome { get; set; }
+    public string Slug { get; set; }
+}
---
+++ API/CodePulse/CodePulse.API/Models/Domain/Post.cs
@@ -0,0 +1,14 @@
+﻿namespace CodePulse.API.Models.Domain;
+
+public class Post
+{
+    public Guid Id { get; set; }
+    public string Titulo { get; set; }
+    public string DescricaoCurta { get; set; }
+    public string Conteudo { get; set; }
+    public string UrlImagemDestaque { get; set; }
+    public string Slug { get; set; }
+    public DateTime DataPublicacao { get; set; }
+    public string Autor { get; set; }
+    public bool EhVisivel { get; set; }
+}
