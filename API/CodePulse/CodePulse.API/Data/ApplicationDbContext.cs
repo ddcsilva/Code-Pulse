78546bba59e3177c1a94eca1e28af86c935526da
danilo.silva <danilo.silva@msn.com>
2024-05-25 21:10:52 -0300

    Criando Contexto

---
+++ API/CodePulse/CodePulse.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CodePulse.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePulse.API.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Categoria> Categorias { get; set; }
+}
