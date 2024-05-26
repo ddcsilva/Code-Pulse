e0fd86d377d74cddeb87ee862d402142fe94b2f3
danilo.silva <danilo.silva@msn.com>
2024-05-26 00:09:28 -0300

    Criando método na Service para Obter Todas as Categorias

---
+++ UI/codepulse/src/app/features/categoria/models/categoria.model.ts
@@ -0,0 +1,5 @@
+export interface Categoria {
+    id: string;
+    nome: string;
+    slug: string;
+}
--- UI/codepulse/src/app/features/categoria/services/categoria.service.ts
+++ UI/codepulse/src/app/features/categoria/services/categoria.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { CriarCategoriaRequest } from '../models/criar-categoria-request.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Categoria } from '../models/categoria.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,10 @@ import { HttpClient } from '@angular/common/http';
 export class CategoriaService {
 
   constructor(private http: HttpClient) { }
+
+  obterTodasCategorias(): Observable<Categoria[]> {
+    return this.http.get<Categoria[]>('https://localhost:7293/api/Categorias');
+  }
 
   adicionarCategoria(model: CriarCategoriaRequest): Observable<void> {
     return this.http.post<void>('https://localhost:7293/api/Categorias', model);
