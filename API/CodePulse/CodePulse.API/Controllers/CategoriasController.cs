de1513bfe0ba72827f41f929d40a2ce08754b0bd
danilo.silva <danilo.silva@msn.com>
2024-05-25 21:16:07 -0300

    Criando Controller de Categoria

---
+++ API/CodePulse/CodePulse.API/Controllers/CategoriasController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers;
+
+public class CategoriasController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
