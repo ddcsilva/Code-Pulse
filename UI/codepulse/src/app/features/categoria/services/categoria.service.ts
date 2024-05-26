2b888b92d57140e1b2e34a0bf0d5192f66c6ff97
danilo.silva <danilo.silva@msn.com>
2024-05-26 00:12:53 -0300

    Criar Environments

--- UI/codepulse/src/app/features/categoria/services/categoria.service.ts
+++ UI/codepulse/src/app/features/categoria/services/categoria.service.ts
@@ -3,6 +3,7 @@ import { CriarCategoriaRequest } from '../models/criar-categoria-request.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Categoria } from '../models/categoria.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +13,10 @@ export class CategoriaService {
   constructor(private http: HttpClient) { }
 
   obterTodasCategorias(): Observable<Categoria[]> {
-    return this.http.get<Categoria[]>('https://localhost:7293/api/Categorias');
+    return this.http.get<Categoria[]>(`${environment.urlApi}/api/Categorias`);
   }
 
   adicionarCategoria(model: CriarCategoriaRequest): Observable<void> {
-    return this.http.post<void>('https://localhost:7293/api/Categorias', model);
+    return this.http.post<void>(`${environment.urlApi}/api/Categorias`, model);
   }
 }
